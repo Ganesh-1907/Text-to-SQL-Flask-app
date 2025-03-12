072f5b131b6076708c6cb5b46dbe7153dc059baf
Ganesh <bora1132004@gmail.com>
2025-03-12 20:22:18 +0530

    Project setup of text to sql task

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, render_template
+import torch
+from transformers import T5Tokenizer, T5ForConditionalGeneration
+
+# Initialize Flask App
+app = Flask(__name__)
+
+# Load Model
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+tokenizer = T5Tokenizer.from_pretrained('t5-small')
+model = T5ForConditionalGeneration.from_pretrained('cssupport/t5-small-awesome-text-to-sql').to(device)
+model.eval()
+
+def generate_sql(input_prompt):
+    inputs = tokenizer(input_prompt, padding=True, truncation=True, return_tensors="pt").to(device)
+    with torch.no_grad():
+        outputs = model.generate(**inputs, max_length=512)
+    return tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    generated_sql = ""
+    if request.method == "POST":
+        user_input = request.form.get("text_input")
+        if user_input:
+            generated_sql = generate_sql(user_input)
+    
+    return render_template("index.html", output_text=generated_sql)
+
+if __name__ == "__main__":
+    app.run(debug=True)
